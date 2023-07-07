f5e7fc5d44939ecb9f8762a46416e07cf62dc2a1
vishnu mishra <vishnumishra8910@gmail.com>
2023-07-07 21:35:07 +0530

    Upated Code for todo list updation on Enter Key

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,9 +7,15 @@ export const TodoInput = (props: any) => {
     props.updateTodoList(todoItem);
     setTodoItem('');
    };
+   const handleKeyup = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+        props.updateTodoList(e.currentTarget.getAttribute('value'));
+        setTodoItem('')
+    }
+  }
     return(
         <div className='input-container'>
-            <input type="text" className='todo-input' placeholder='enter todo here' value={todoItem} onChange={(event)=> setTodoItem(event.target.value)}/>
+            <input type="text" className='todo-input' placeholder='enter todo here' value={todoItem} onKeyUp={(event) => { event.key === "Enter" && handleKeyup(event)} } onChange={(event)=> setTodoItem(event.target.value)}/>
             <button className='add-todo-btn' onClick={()=> todoItem && addTodoItem()}>+</button>
         </div>
     );
